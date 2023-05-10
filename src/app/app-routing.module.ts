84c0b50f780d0b1f6edda04a83554713c7724b39
LinaNieto <lina.nieto22243@ucaldas.edu.co>
2023-05-10 17:24:17 -0500

    Creating a page to register as an advisor or as a client.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
 import { PathNotFoundComponent } from './public/errors/path-not-found/path-not-found.component';
+import { RegisterSwitchComponent } from './public/register-switch/register-switch.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
     path: '', // Empty path
     pathMatch: 'full',
     redirectTo: '/home',
+  },
+  {
+    path:'register-switch',
+    component: RegisterSwitchComponent
   },
   {
     path: 'security', // Security module path
