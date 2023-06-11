196587e98c02953667611e5d6c73192816cb7d3e
Kevin Lopez <reportzack@gmai.com>
2023-06-11 15:25:12 -0500

    Created Edit Department Page

--- src/app/modules/parameters/department/edit-department/edit-department.component.ts
+++ src/app/modules/parameters/department/edit-department/edit-department.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { RolesConfig } from 'src/app/config/roles.config';
+import { RoutesBackendConfig } from 'src/app/config/routes-backend.config';
+import { AdviserModel } from 'src/app/models/Adviser.model';
+import { CityModel } from 'src/app/models/city.model';
+import { DepartmentModel } from 'src/app/models/department.model';
+import { DepartmentService } from 'src/app/services/parameters/department.service';
+import { SecurityService } from 'src/app/services/security.service';
 
 @Component({
   selector: 'app-edit-department',
@@ -6,5 +15,94 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-department.component.css']
 })
 export class EditDepartmentComponent {
+  dataFG: FormGroup = new FormGroup({});
+  logicUrl: String = RoutesBackendConfig.urlBusinessLogic;
+  department: DepartmentModel = {};
+  departmentId = 0;
 
+  constructor(
+    private fb: FormBuilder,
+    private service: DepartmentService,
+    private router: Router,
+    private security: SecurityService,
+    private route: ActivatedRoute
+  ) {
+    this.departmentId = this.route.snapshot.params['id'];
+  }
+
+  ngOnInit() {
+    let data = this.security.sessionValidation();
+    if (data != null) {
+      if (
+        data.user?.roleId == RolesConfig.administratorId
+      ) {
+        this.getDepartment();
+        this.buildDataFG();
+      } else {
+        this.router.navigate(['']);
+      }
+    } else {
+      this.router.navigate(['']);
+    }
+  }
+
+  buildDataFG() {
+    this.dataFG = this.fb.group({
+      name: ['', [Validators.required]],
+    });
+  }
+
+  replaceDataFG() {
+    this.dataFG.get('name')?.patchValue(this.department.name);
+  }
+
+  getDepartment() {
+    let filter = {
+      where: {
+        id: this.departmentId,
+      },
+    };
+    this.service.getDepartments(filter).subscribe({
+      next: (data) => {
+        this.department = data.records[0];
+        console.log(this.department);
+        this.replaceDataFG();
+      },
+      error: (err: any) => {
+        return (err = true);
+      },
+    });
+  }
+
+  editDepartment() {
+    if (this.dataFG.invalid) {
+      alert('Incomplete Data');
+    } else {
+      let model = this.get();
+      this.service.editDepartment(model).subscribe({
+        next: (data: DepartmentModel) => {
+          alert('department Edited Successfully');
+          this.router.navigate(['/parameters/list-department']);
+        },
+        error: (err: any) => {
+          alert('An Error has happened creating the department');
+        },
+      });
+    }
+  }
+
+  get(): DepartmentModel {
+    let model = new DepartmentModel();
+    model.id = this.department.id;
+    model.name = this.getDataFG['name'].value;
+    console.log(model);
+    if (this.dataFG.invalid) {
+      alert('Incomplete Data');
+    }
+    return model;
+  }
+
+  get getDataFG() {
+    return this.dataFG.controls;
+  }
 }
